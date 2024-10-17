e5fc50a5279f97e5e6d9fc37fec9f3e5f56903fb
ArchanaHarikrishnan <archana18harikrishnan@gmail.com>
2024-10-17 13:11:12 +0530

    Add project files.

---
+++ DivideByZeroException/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DivideByZeroExceptioneEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numerator: "); 
+            int numerator=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Denominator:");
+            int denominator=int.Parse(Console.ReadLine());
+
+            try
+            {
+                int result = numerator / denominator;
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
